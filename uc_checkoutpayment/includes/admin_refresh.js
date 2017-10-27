1328d23c18b4a0917eff133dbdcc76c3be539d33
gilles-coeman-cko <gilles.coeman@checkout.com>
2017-10-27 14:38:59 +0100

    Refresh admin page

---
+++ uc_checkoutpayment/includes/admin_refresh.js
@@ -0,0 +1,22 @@
+var interval = 1000;
+var action = document.getElementById("cko-action").value;
+var contextid = document.getElementById("order_id").value;
+
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      setTimeout(loadDoc, interval);
+      if(this.responseText == 1){
+        location.reload();
+      }
+    }
+  };
+  xhttp.open("GET", "/?q=/uc_checkoutpayment/admin/" + action + "/" + contextid, true);
+  xhttp.send();
+}
+
+if(action == 'capture' || action == 'refund'){
+    setTimeout(loadDoc, interval);
+}
+
